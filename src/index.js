ab646cffa8acc324cf8e34550ce57695c789dcf7
MonseCuevas <monsis.cuevas@gmail.com>
2019-06-14 00:48:34 -0500

    Maquetado desencriptar

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 document.getElementById("btnlado").addEventListener("click", ()=> {
     document.getElementById("idInicio").style.display = "none";
     document.getElementById("idEncriptado").style.display = "block";
-  });
+    document.getElementById("idDesencriptado").style.display = "none";
+ });
+
   
-  document.getElementById("aEncriptar").addEventListener("click", () => {
+document.getElementById("aEncriptar").addEventListener("click", () => {
     document.getElementById("idInicio").style.display = "none";
     document.getElementById("idEncriptado").style.display = "block";
-    document.getElementById("aInicio").addEventListener("click",() => {
-  console.log("hola")
-    });
-    
-  
+    document.getElementById("idDesencriptado").style.display = "none";
+  });
+
+document.getElementById("aInicio").addEventListener("click", () => {
+      document.getElementById("idEncriptado").style.display = "none";
+      document.getElementById("idInicio").style.display = "block";
+      document.getElementById("idDesencriptado").style.display = "none";
+  });
+document.getElementById("aDesencriptar").addEventListener("click", () => {
+      document.getElementById("idEncriptado").style.display = "none";
+      document.getElementById("idDesencriptado").style.display = "block";
+      document.getElementById("idInicio").style.display = "none";
+ });
