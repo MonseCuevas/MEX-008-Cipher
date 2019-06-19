94a57637a1a0efc97c33b9b25cf7af8309b55b60
MonseCuevas <monsis.cuevas@gmail.com>
2019-06-19 13:26:34 -0500

    agrege cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,40 @@
+function mod(n, m) {
+  return ((n % m) + m) % m;
+}
+
 window.cipher = {
-  // ... 
+  encode: (mensaje, offset) => {
+    let mensajeCifrado = '';
+
+    for (let i = 0; i < mensaje.length; i++) {
+      let caracter = mensaje.charAt(i);
+
+      if(caracter === ' '){
+        mensajeCifrado = mensajeCifrado + caracter;
+      } else {
+        let numeroASCII = caracter.charCodeAt(0);
+        let numeroOffset = ((numeroASCII - 65 + parseInt(offset)) % 26) + 65;
+        mensajeCifrado = mensajeCifrado + String.fromCharCode(numeroOffset);
+      }
+    }
+
+    return mensajeCifrado;
+  },
+  decode: (mensaje, offset) => {
+    let mensajeDesencriptado = '';
+
+    for(let i = 0; i < mensaje.length; i++){
+      let caracter = mensaje.charAt(i);
+
+      if(caracter === ' '){
+        mensajeDesencriptado = mensajeDesencriptado + caracter;
+      } else {
+        let numeroASCII = caracter.charCodeAt(0);
+        let numeroOffset = mod((numeroASCII - 65 - parseInt(offset)), 26) + 65;
+        mensajeDesencriptado = mensajeDesencriptado + String.fromCharCode(numeroOffset);
+      }
+    }
+
+    return mensajeDesencriptado;
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,23 +1,45 @@
 document.getElementById("btnlado").addEventListener("click", ()=> {
-    document.getElementById("idInicio").style.display = "none";
-    document.getElementById("idEncriptado").style.display = "block";
-    document.getElementById("idDesencriptado").style.display = "none";
- });
-
+  document.getElementById("idInicio").style.display = "none";
+  document.getElementById("idEncriptado").style.display = "block";
+  document.getElementById("idDesencriptado").style.display = "none";
+});
   
 document.getElementById("aEncriptar").addEventListener("click", () => {
-    document.getElementById("idInicio").style.display = "none";
-    document.getElementById("idEncriptado").style.display = "block";
-    document.getElementById("idDesencriptado").style.display = "none";
-  });
+  document.getElementById("idInicio").style.display = "none";
+  document.getElementById("idEncriptado").style.display = "block";
+  document.getElementById("idDesencriptado").style.display = "none";
+});
 
 document.getElementById("aInicio").addEventListener("click", () => {
-      document.getElementById("idEncriptado").style.display = "none";
-      document.getElementById("idInicio").style.display = "block";
-      document.getElementById("idDesencriptado").style.display = "none";
-  });
+  document.getElementById("idEncriptado").style.display = "none";
+  document.getElementById("idInicio").style.display = "block";
+  document.getElementById("idDesencriptado").style.display = "none";
+});
+
 document.getElementById("aDesencriptar").addEventListener("click", () => {
-      document.getElementById("idEncriptado").style.display = "none";
-      document.getElementById("idDesencriptado").style.display = "block";
-      document.getElementById("idInicio").style.display = "none";
- });
+  document.getElementById("idEncriptado").style.display = "none";
+  document.getElementById("idDesencriptado").style.display = "block";
+  document.getElementById("idInicio").style.display = "none";
+});
+
+document.getElementById("btnEncriptado").addEventListener("click", () => {
+  let offset = document.getElementById("offsetEncriptado").value;
+  let lada = document.getElementById("ladaEncriptado").value;
+  let numero = document.getElementById("numeroEncriptado").value;
+  let mensajeAEncriptar = document.getElementById("mensajeAEncriptar").value;
+  let mensajeEncriptado = document.getElementById("mensajeEncriptado");
+
+  let mensajeCifrado = cipher.encode(mensajeAEncriptar.trim(), offset);
+  mensajeEncriptado.value = mensajeCifrado;
+
+  window.open("https://wa.me/" + lada.trim() + numero.trim() + "?text=" + mensajeCifrado, "_blank");
+});
+
+document.getElementById("btnDesencriptar").addEventListener("click", () => {
+  let offset = document.getElementById("offsetDesencriptado").value;
+  let mensajeADesencriptar = document.getElementById("mensajeADesencriptar").value;
+  let mensajeDesencriptado = document.getElementById("mensajeDesencriptado");
+
+  let mensajeDecifrado = cipher.decode(mensajeADesencriptar, offset);
+  mensajeDesencriptado.value = mensajeDecifrado;
+});
